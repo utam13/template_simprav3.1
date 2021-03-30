be13f4c8d65b223533c5a83fae65ed82c0c55863
rustam13 - <rustam.blacknetto@gmail.com>
2021-03-31 05:41:05 +0800

    perubahan header dan aksi tombol menu untuk menampilkan serta menyembunyikan sidebar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-// $(document).on("resize", function () {
-// 	if ($(document).height() > 800) {
-// 		alert("tes");
-// 	} else {
-// 		alert("tes");
-// 	}
-// });
-
-
+$('.btn-menu').click(function () {
+    if ($('.sidepanel').hasClass('sr-only') == false) {
+        $('.sidepanel').addClass('sr-only');
+        $('.content-panel').removeClass('col-lg-9');
+        $('.content-panel').addClass('col-lg-12');
+        $('.card').addClass('ml-3');
+    } else {
+        $('.sidepanel').removeClass('sr-only');
+        $('.content-panel').removeClass('col-lg-12');
+        $('.card').removeClass('ml-3');
+        $('.content-panel').addClass('col-lg-9');
+    }
+})
