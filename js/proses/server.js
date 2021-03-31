3e1d5b0e16aed5d2a9f9be3341f8837d20eb1a85
rustam13 - <rustam.blacknetto@gmail.com>
2021-03-31 13:56:50 +0800

    menambahkan css color, penambahan style di css, penambahan js server dan dashboard, menambahkan halaman dashboard

---
+++ js/proses/server.js
@@ -0,0 +1,8 @@
+// -----------------------------------------------------------------------------------------
+// atur lokasi server
+const loc = "http://localhost/simprav3.1/";
+// const serverLoc = loc + 'landing/';
+
+let thn_sekarang = 2021;
+let list_thn = thn_sekarang.split('-');
+console.log(list_thn);
