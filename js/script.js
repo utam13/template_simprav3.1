df5ecac4b8e509af2680edd70b368ac42c22857e
rustam13 - <rustam.blacknetto@gmail.com>
2021-04-04 11:09:06 +0800

    perbaikan breadcrumb dan penambahan card info disetiap halaman

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ $('.btn-menu').click(function () {
         $('.content-panel').removeClass('col-lg-9');
         $('.content-panel').addClass('col-lg-12');
         $('.card').addClass('ml-3');
+        $('.nav-breadcrumb').addClass('ml-3');
     } else {
         $('.sidepanel').removeClass('sr-only');
         $('.content-panel').removeClass('col-lg-12');
         $('.card').removeClass('ml-3');
         $('.content-panel').addClass('col-lg-9');
+        $('.nav-breadcrumb').removeClass('ml-3');
     }
 })
 
